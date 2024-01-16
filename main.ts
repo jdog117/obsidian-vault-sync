b04087cdd1e824d98368748836d53cbce179fa59
Joe Casey <joeyc@LAPTOP-982L04VI.localdomain>
2024-01-16 15:07:02 -0800

    log current file to console

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
 import { Plugin } from 'obsidian';
 
 export default class AWSSync extends Plugin {
-    onLoad(): void {
-        
+
+    async onLoad() {
+        // Configure resources needed by the plugin.
+        console.log('loading plugin')
+        const openFile = this.app.workspace.getActiveFile();
+        if(openFile) {
+            console.log(openFile.name)
+            console.log(openFile.path)
+            //console.log(openFile.stat.mtime)
+        }
     }
+
+    async onunload() {
+        // Release any resources configured by the plugin.
+        console.log('unloading plugin')
+      }
+
 }
