39abb67e17849250190525e8a6c54f2ab05c5b83
Joe Casey <joeyc@LAPTOP-982L04VI.localdomain>
2024-01-24 21:57:21 -0800

    jus muckin around

--- main.ts
+++ main.ts
@@ -1,13 +1,33 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
+require('dotenv').config();
+
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+console.log(accessKeyId)
 
 export default class Sync extends Plugin {
 
     async onload() {
         console.log('loading plugin')
-        const openFile = this.app.vault.getFiles();
-        if(openFile) {
-            //console.log(openFile)
-        }
+
+        this.addRibbonIcon("upload-cloud", "AWS Sync", () => {
+            //save function here
+            this.copyFiles
+
+        })
+
+        this.addCommand({
+            id:"save",
+            name: "Save to cloud",
+            callback: () => {
+                new Notice("Working!");
+                //can bind commands to keyboard shortcut
+
+                //If you want checking peek below
+                //https://www.youtube.com/live/Hn-ozsctckQ?si=G9oLYFirOOzDJ2WC
+            }
+        })
+
     }
 
     async onunload() {
@@ -15,4 +35,14 @@ export default class Sync extends Plugin {
         console.log('unloading plugin')
       }
 
+
+      //save
+      copyFiles() {
+        console.log("burv")
+        const getVaultFiles = this.app.vault.getFiles();
+        if(getVaultFiles) {
+            console.log(getVaultFiles)
+        }
+
+      }
 }
