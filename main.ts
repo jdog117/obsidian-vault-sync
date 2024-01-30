90971f59799f760c9ca4dfb2401911d18589b6d1
Joe Casey <joeyc@LAPTOP-982L04VI.localdomain>
2024-01-29 17:43:45 -0800

    initial s3 upload code

--- main.ts
+++ main.ts
@@ -1,18 +1,30 @@
 import { Notice, Plugin } from 'obsidian';
-
+import { 
+    DeleteObjectCommand,
+    GetObjectCommand,
+    HeadBucketCommand,
+    HeadObjectCommand,
+    HeadObjectCommandOutput,
+    ListObjectsV2Command,
+    ListObjectsV2CommandInput,
+    S3Client, 
+    PutObjectCommand 
+} from '@aws-sdk/client-s3';
 
 export default class Sync extends Plugin {
 
     async onload() {
         console.log('loading plugin')
 
+        //ICON
         this.addRibbonIcon("upload-cloud", "AWS Sync", () => {
             //save function here
-            const getVaultFiles = this.app.vault.getMarkdownFiles();
-            this.copyFiles(getVaultFiles)
+            const vaultFiles = this.app.vault.getMarkdownFiles();
+            this.uploadToS3(vaultFiles, "bucketname","key");
 
         })
 
+        //COMMAND
         this.addCommand({
             id:"save",
             name: "Save to cloud",
@@ -24,19 +36,38 @@ export default class Sync extends Plugin {
 
     }
 
+    //s3 upload method
+    private async uploadToS3(vaultFiles, bucketName, key) {
+
+        //setup aws s3
+        const s3Client = new S3Client({
+            region: 'MYREGION',
+            credentials: {
+                accessKeyId: "MYACCESSKEYID",
+                secretAccessKey: "MYSECRETACCESSKEY",
+            },
+        });
+        
+        const params = {
+            Bucket: bucketName,
+            Key: key,
+            Body: vaultFiles,
+          };
+
+          try {
+            const command = new PutObjectCommand(params);
+            const response = await s3Client.send(command);
+            console.log('File uploaded successfully:', response);
+          } catch (error) {
+            console.error('Error uploading file to S3:', error);
+          }
+    }
+
+
     async onunload() {
 
         // Release any resources configured by the plugin.
         console.log('unloading plugin')
-
-      }
-
-
-    //save
-    private copyFiles(vaultFiles?) {
-
-        console.log(vaultFiles[1])
-        //vaultFiles[1].copy()
 
       }
 
