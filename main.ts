3a863f176bf661acd5aa5dbddee7bd68588f644b
Joe Casey <joeyc@LAPTOP-982L04VI.localdomain>
2024-01-29 16:41:31 -0800

    clean up

--- main.ts
+++ main.ts
@@ -1,15 +1,7 @@
 import { Notice, Plugin } from 'obsidian';
 
-export interface DataAdapter {
-    copy(normalizedPath: string, normalizedNewPath: string);
-    //copy(normalizedPath: "TODO.md", normalizedNewPath: "C:\\Users\\jspca\\Desktop\\");
-}
 
-export default class Sync extends Plugin implements DataAdapter{
-
-    //implement copy method from DataAdapter implementation
-    copy(normalizedPath: string, normalizedNewPath: string): void {
-    }
+export default class Sync extends Plugin {
 
     async onload() {
         console.log('loading plugin')
@@ -26,28 +18,25 @@ export default class Sync extends Plugin implements DataAdapter{
             name: "Save to cloud",
             callback: () => {
                 new Notice("Working!");
-                //can bind commands to keyboard shortcut
 
-                //If you want checking peek below
-                //https://www.youtube.com/live/Hn-ozsctckQ?si=G9oLYFirOOzDJ2WC
             }
         })
 
     }
 
     async onunload() {
+
         // Release any resources configured by the plugin.
         console.log('unloading plugin')
+
       }
 
 
-      //save
+    //save
     private copyFiles(vaultFiles?) {
 
         console.log(vaultFiles[1])
         //vaultFiles[1].copy()
-        
-        this.copy("~\TODO.md", "C:/Users/jspca/Desktop/TODO.md");
 
       }
 
