bcbc455fa58b21d12ba07549957db6c1db6aab22
jdog117 <jspcasey@gmail.com>
2024-05-18 23:31:02 -0700

    sync decoupling
    decoupled sync from aws and added some adapters

--- sync.ts
+++ sync.ts
@@ -17,23 +17,64 @@ const s3Client = new S3Client({
     },
 });
 
-export class Sync {
-    vault: Vault;
+interface IVault {
+    adapter: {
+        write: (name: string, content: any) => Promise<void>;
+        read: (path: string) => Promise<any>;
+    };
+}
 
-    constructor(vault: Vault) {
+interface IStorageClient {
+    getObjects: (bucketName: string) => Promise<any>;
+    upload: (filesWithContent: any, bucketName: string) => void;
+}
+
+// adapter for vault interface
+class VaultAdapter implements IVault {
+    adapter: {
+        write: (name: string, content: any) => Promise<void>;
+        read: (path: string) => Promise<any>;
+    };
+
+    constructor(private vault: Vault) {
+        this.adapter = {
+            write: (name: string, content: any) =>
+                vault.adapter.write(name, content),
+            read: (path: string) => vault.adapter.read(path),
+        };
+    }
+}
+
+// adapter for storage interface
+class S3ClientAdapter implements IStorageClient {
+    getObjects: (bucketName: string) => Promise<any>;
+    upload: (filesWithContent: any, bucketName: string) => void;
+
+    constructor(private s3Client: S3Client) {
+        this.getObjects = (bucketName: string) =>
+            getS3Objects(bucketName, s3Client);
+        this.upload = (filesWithContent: any, bucketName: string) =>
+            uploadToS3(filesWithContent, bucketName, s3Client);
+    }
+}
+
+export class Sync {
+    vault: IVault;
+    storageClient: IStorageClient;
+
+    constructor(vault: IVault, storageClient: IStorageClient) {
         this.vault = vault;
+        this.storageClient = storageClient;
     }
 
-    //writes all files from s3 to the vault
     async writeVaultFiles() {
-        const pulledFiles = await getS3Objects(BUCKET_NAME, s3Client);
+        const pulledFiles = await this.storageClient.getObjects(BUCKET_NAME);
         console.log(pulledFiles);
         for (const obj of pulledFiles) {
             await this.vault.adapter.write(obj.name, obj.content);
         }
     }
 
-    //uploads all files from the vault to s3
     async uploadVault(vaultFiles) {
         Promise.all(
             vaultFiles.map(async (file) => {
@@ -42,13 +83,18 @@ export class Sync {
                 return { path: file.path, content };
             })
         ).then((filesWithContent) => {
-            uploadToS3(filesWithContent, BUCKET_NAME, s3Client);
+            this.storageClient.upload(filesWithContent, BUCKET_NAME);
         });
     }
 
-    async mainSyncButton() {
-        const vaultFiles = this.vault.getMarkdownFiles();
-        //await this.uploadVault(vaultFiles);
+    async mainSyncButton(vaultFiles) {
+        // create save logic, only use upload or wite once at a time for now
+        await this.uploadVault(vaultFiles);
         await this.writeVaultFiles();
     }
 }
+
+const vault = new VaultAdapter(new Vault());
+const storageClient = new S3ClientAdapter(s3Client);
+
+const sync = new Sync(vault, storageClient);
